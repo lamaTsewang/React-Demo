87a3be997dcb3857ef548eaef40247127eba0b94
tsewangtamba@gmail.com <tsewangtamb@gmail.com>
2022-11-26 23:06:12 +0545

    textform edited

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -50,7 +50,7 @@ export default function TextForm(props) {
             <h1>{props.heading}</h1>
             {/* </div> */}
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: 
+            <textarea className="form-control" selector="#editor" value={text} onChange={handleOnChange} style={{backgroundColor: 
             props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
